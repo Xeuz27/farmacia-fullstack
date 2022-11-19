7816f8f7f45f4edc4a354a61749b954447dd3f15
Jesus Gutierrez <86269566+Xeuz27@users.noreply.github.com>
2022-11-19 14:31:18 -0400

    pa que rome haga pull /frontend

---
+++ src/views/auth/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Row, Col, Card, } from 'react-bootstrap';
+import Header from '../../components/Header';
+
+export default function Login() {
+    return (<>
+
+        
+    </>)
+}
